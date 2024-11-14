37b801ce2e5ff4c0aa45b4f7e1e1d1edce95308c
yasmin <yasminjefri30@gmail.com>
2024-11-14 11:53:34 +0800

    2. Add comment on line 1

---
+++ index.js
@@ -0,0 +1,17 @@
+//document.getElementById("count-el").innerText = 115;
+
+let count = 0;
+let countEl = document.getElementById("count-el");
+
+function increment() {
+    count = count + 1;
+    countEl.innerText = count;
+}
+
+function save() {
+    let countStr = count + " - ";
+    let message = document.getElementById("save-el");
+    message.textContent += countStr;
+    count = 0;
+}
+
